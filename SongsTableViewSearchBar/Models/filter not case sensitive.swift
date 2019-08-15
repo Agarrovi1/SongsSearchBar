5aff0235655819c6a3925ac867cd64338222ad7c
Angela Garrovillas <angelagarrovillas@Angelas-MacBook-Pro.local>
2019-08-15 16:24:28 -0400

    adds function to filter so case does not matter

---
+++ SongsTableViewSearchBar/Models/filter not case sensitive.swift
@@ -0,0 +1,9 @@
+//
+//  filter not case sensitive.swift
+//  SongsTableViewSearchBar
+//
+//  Created by Angela Garrovillas on 8/15/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import Foundation
